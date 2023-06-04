b66c379f0eefd979fa51c5c3750222d87c92b50a
Andre Theofilus Permana <91383519+Unitevers@users.noreply.github.com>
2023-06-04 22:16:31 +0700

    fix new navbar (tolong dicek)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.new_main')
 
 @section('contents')
     <h1>Welcome to StocKing</h1>
--- resources/views/layouts/new_main.blade.php
+++ resources/views/layouts/new_main.blade.php
@@ -11,18 +11,22 @@
   </head>
   <body>
     <nav>
-        <a href="WelcomeGuest.html"><img src="/img/stocking-logo.png"></a>
         <div class="navbar">
-            <ul>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
-            </ul>
+            <div class="logo">
+                <a href=""><img src="/img/stocking-logo.png"></a>
+            </div>
+            <div class="navbar-menu">
+                <ul>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
     @yield('contents')
-    <div class="footer">
+    {{-- <div class="footer">
         <p>&copy;StocKing</p>
-    </div>
+    </div> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
     crossorigin="anonymous"></script>
