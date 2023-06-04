dd7551197166671619252881db8564b3702272ce
Andre Theofilus Permana <91383519+Unitevers@users.noreply.github.com>
2023-06-04 23:19:39 +0700

    Navbar dah 100% working as intended, masukkin welcoming guest (WIP)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.new_main')
 
 @section('contents')
-    <h1>Welcome to StocKing</h1>
+<link rel="stylesheet" href="/css/homeStyle.css">
+<div class="container">
+    <div class="content1">
+        <div class="left-content1">
+            <h1>
+                StocKing
+            </h1>
+            <p>
+                Make your inventory management easier
+            </p>
+            <button class="register" type="button">Register Now</button>
+        </div>
+        <div class="right-content1">
+            <img src="">
+        </div>
+    </div>
+    <div class="content2">
+        <h3>
+            About
+        </h3>
+        <div class="rectangle-content2"></div>
+        <p>
+            StocKing is a web application that provides the services to help user and simplify the process of managing perishable goods.
+        </p>
+    </div>
+    <div class="content3">
+        <h3>
+            What's new?
+        </h3>
+        <div class="rectangle-content3"></div>
+        <p>
+            Celebrating the version 1.0 release, all subscription plan will be 50% off till june 14th
+        </p>
+    </div>
+</div>
 @endsection
