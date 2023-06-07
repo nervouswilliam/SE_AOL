1783f9e6ad4526aff2cce85001cb29f0f0054e04
fakhrifend <fakhrifendiansyah@gmail.com>
2023-06-07 22:14:44 +0700

    editing regist, login, menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -11,11 +11,6 @@
             <div class="middle">
                 <h2>INSERT PRODUCT</h2>
             </div>
-            <div class="lower">
-                <h3>
-                    You can insert your product
-                </h3>
-            </div>
         </div>
 
         <div class="column">
@@ -24,9 +19,6 @@
             </div>
             <div class="middle">
                 <h2>EXTRACT PRODUCT</h2>
-            </div>
-            <div class="lower">
-                <h3>You can remove your product</h3>
             </div>
         </div>
 
@@ -37,9 +29,6 @@
             <div class="middle">
                 <h2>VIEW INVENTORY</h2>
             </div>
-            <div class="lower">
-                <h3>You can view your product in inventory</h3>
-            </div>
         </div>
 
         <div class="column">
@@ -48,9 +37,6 @@
             </div>
             <div class="middle">
                 <h2>INVENTORY REPORT</h2>
-            </div>
-            <div class="lower">
-                <h3>You can view your inventory report</h3>
             </div>
         </div>
     </div>
